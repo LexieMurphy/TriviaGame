98c190b48d6eab32ac133e1ea2b851bd5c225e0f
Lexie_Murphy <Lexie.Murphy88@gmail.com>
2018-12-21 16:48:31 -0600

    got timer working by moving out the function

--- assets/javascript/app - Copy.js
+++ assets/javascript/app - Copy.js
@@ -83,7 +83,7 @@ function reset() {
 
 // start the count and set clock to running (counting down).
 function letsPlay() {
-    if(!clockRunning) {
+    if (!clockRunning) {
         reset();
         intervalId = setInterval(count, 1000);
         clockRunning = true;
@@ -104,10 +104,10 @@ function count() {
     console.log(converted);
     $("#timeDisplay").text(converted);
     console.log(time);
-    if(time < 1) {
-    showResults(questions, quizContainer, resultsContainer);
-    stop();
-    alert("Time is OUT!");
+    if (time < 1) {
+        showResults(myQuestions, quizContainer, resultsContainer);
+        stop();
+        alert("Time is OUT!");
     }
 }
 
@@ -116,104 +116,107 @@ function timeConverter(t) {
 
     var minutes = Math.floor(t / 60);
     var seconds = t - (minutes * 60);
-  
+
     if (seconds < 10) {
-      seconds = "0" + seconds;
+        seconds = "0" + seconds;
     }
-  
+
     if (minutes === 0) {
-      minutes = "00";
+        minutes = "00";
     }
     else if (minutes < 10) {
-      minutes = "0" + minutes;
+        minutes = "0" + minutes;
     }
-  
+
     return minutes + ":" + seconds;
-  }
-  
+}
+
 
 
 generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
 
 function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
-
-    function showQuestions(questions, quizContainer) {
-        // we'll need a place to store the output and the answer choices
-        var output = [];
-        var answers;
-
-        // for each question...
-        for (var i = 0; i < questions.length; i++) {
-
-            // first reset the list of answers
-            answers = [];
-
-            // for each available answer...
-            for (letter in questions[i].answers) {
-
-                // ...add an html radio button
-                answers.push(
-                    '<label>'
-                    + '<input type="radio" name="question' + i + '" value="' + letter + '">'
-                    + " " + letter + ': '
-                    + questions[i].answers[letter]
-                    + '</label>'
-                );
-            }
-
-            // add this question and its answers to the output
-            output.push(
-                '<div class="question">' + questions[i].question + '</div>'
-                + '<div class="answers">' + answers.join('  ') + '</div>'
-            );
-        }
-
-        // finally combine our output list into one string of html and put it on the page
-        quizContainer.innerHTML = output.join('');
-    }
-
-
-    function showResults(questions, quizContainer, resultsContainer) {
-
-        // gather answer containers from our quiz
-        var answerContainers = quizContainer.querySelectorAll('.answers');
-
-        // keep track of user's answers
-        var userAnswer = '';
-        var numCorrect = 0;
-
-        // for each question...
-        for (var i = 0; i < questions.length; i++) {
-
-            // find selected answer
-            userAnswer = (answerContainers[i].querySelector('input[name=question' + i + ']:checked') || {}).value;
-
-            // if answer is correct
-            if (userAnswer === questions[i].correctAnswer) {
-                // add to the number of correct answers
-                numCorrect++;
-
-                // color the answers green
-                answerContainers[i].style.color = 'lightgreen';
-            }
-            // if answer is wrong or blank
-            else {
-                // color the answers red
-                answerContainers[i].style.color = 'red';
-            }
-        }
-
-        // show number of correct answers out of total
-        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
-    }
-
-    // show questions right away
-    showQuestions(questions, quizContainer);
-
+    showQuestions(myQuestions, quizContainer);
     // on submit, show results
     submitButton.onclick = function () {
         showResults(questions, quizContainer, resultsContainer);
         stop();
     }
-
 }
+
+function showQuestions(questions, quizContainer) {
+    // we'll need a place to store the output and the answer choices
+    var output = [];
+    var answers;
+
+    // for each question...
+    for (var i = 0; i < questions.length; i++) {
+
+        // first reset the list of answers
+        answers = [];
+
+        // for each available answer...
+        for (letter in questions[i].answers) {
+
+            // ...add an html radio button
+            answers.push(
+                '<label>'
+                + '<input type="radio" name="question' + i + '" value="' + letter + '">'
+                + " " + letter + ': '
+                + questions[i].answers[letter]
+                + '</label>'
+            );
+        }
+
+        // add this question and its answers to the output
+        output.push(
+            '<div class="question">' + questions[i].question + '</div>'
+            + '<div class="answers">' + answers.join('  ') + '</div>'
+        );
+    }
+
+    // finally combine our output list into one string of html and put it on the page
+    quizContainer.innerHTML = output.join('');
+}
+
+
+function showResults(questions, quizContainer, resultsContainer) {
+
+    // gather answer containers from our quiz
+    var answerContainers = quizContainer.querySelectorAll('.answers');
+
+    // keep track of user's answers
+    var userAnswer = '';
+    var numCorrect = 0;
+
+    // for each question...
+    for (var i = 0; i < questions.length; i++) {
+
+        // find selected answer
+        userAnswer = (answerContainers[i].querySelector('input[name=question' + i + ']:checked') || {}).value;
+
+        // if answer is correct
+        if (userAnswer === questions[i].correctAnswer) {
+            // add to the number of correct answers
+            numCorrect++;
+
+            // color the answers green
+            answerContainers[i].style.color = 'lightgreen';
+        }
+        // if answer is wrong or blank
+        else {
+            // color the answers red
+            answerContainers[i].style.color = 'red';
+        }
+    }
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+}
+
+    // show questions right away
+    //showQuestions(questions, quizContainer);
+
+
+
+
